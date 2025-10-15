7a17d0030581b9930726825d287e3f04d5b1e54c
UP240594 <up240594@alumnos.upa.edu.mx>
2025-10-15 13:17:33 -0600

    vimos un ArrayList, y importamos su libreria para poder usarlo. si le entendi

---
+++ e22AlumnoAsiento.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class e22AlumnoAsiento{
+    public static void main(String[] args){
+        int[] numerosPares = new int[4];
+        numerosPares[0] = 2;
+        numerosPares[1] = 4;
+        numerosPares[2] = 6;
+        numerosPares[3] = 8;
+
+        ArrayList<Integer> numerosParesMejorados = new ArrayList<Integer>(3); // Tambien, no es necesario poner el numero 3
+        // Voy a crear un estuche de algo
+        //Sirven para hacer crecer su tamaño
+        //es una estructura para controlar un conjunto de INTERGERS
+        numerosParesMejorados.add(2); //Aqui estamos agregando el numero 3 al array
+        numerosParesMejorados.add(4);  //Pueden hacer crecer su conjunto.
+        numerosParesMejorados.add(6);
+        numerosParesMejorados.add(6);
+        numerosParesMejorados.add(6);
+        numerosParesMejorados.add(6);
+    }
+}
+
