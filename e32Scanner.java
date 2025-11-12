0df5bb9ef8d0a83f8392554fa1472eb603bc9f91
UP240594 <up240594@alumnos.upa.edu.mx>
2025-11-12 12:21:27 -0600

    Hice muchas cosas per olvide subirlas parte 2 xd

---
+++ e32Scanner.java
@@ -0,0 +1,5 @@
+class e32Scanner{
+    public static void main(String[] args){
+
+    }
+}
---
+++ ejercicioPractica1.java
@@ -0,0 +1,23 @@
+class ejercicioPractica1{
+    public static void main(String[] args){
+
+    }
+}
+
+class Persona66{
+    String nombre;
+    int edad;
+}
+
+class Usuario66{
+    String password;
+
+    Usuario66(String pass){
+        this.password=pass;
+    }
+
+    boolean login(String password){
+        return true;
+    }
+
+}
